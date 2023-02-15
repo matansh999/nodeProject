931bcf27b9414d6f2e4dc0d79f9548de840c8bf5
matansh999 <45884996+matansh999@users.noreply.github.com>
2023-02-15 14:58:38 +0200

    del

--- index.js
+++ index.js
@@ -1,63 +1,44 @@
-const express = require('express'); //Import the express dependency
-const port = 7777;                  //Save the port number where your server will be listening
-const app = express();//Instantiate an express app, the main work horse of this server
-const server = require('http').createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const router = express.Router();
+var player ="red";
 
+app.set('view engine', 'ejs');
 
-router.get('/',function(req,res){
-  res.render("index", {
-      cunt:639
-    });
-}); 
+// Define a route for the game page.
+app.get('/game', (req, res) => {
+  res.render('game.ejs');
+});
+app.get('/', (req, res) => {
+  res.render('game');
+});
 
-io.on('connection', (socket) => {
+io.on('connection', function(socket) {
   console.log('User connected with ID:', socket.id);
-});
-
-
-router.post('/login',function(req,res){
-  const { uname, passwd } = req.body;
-
-  if (uname === "admin" && passwd === "admin") {
-    res.render("success", {
-      uname: uname,
-    });
-  } else {
-    err++;
-    res.render("login", {
-      uname: uname,
-      err: err,
-    });
+  socket.emit("player",player);
+  if(player=="red"){
+    player="yellow"
   }
+  else{
+    player="red"
+  }
+  socket.on("enter",()=>{
+    if(player=="red"){
+      player="yellow"
+    }
+    else{
+      player="red"
+    }
+    socket.emit("turn",player);
+  })
+
 });
 
-
-//add the router
-app.use('/', router);
-
-
-server.listen(port,() => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
-});
-
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
-
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+server.listen(7777, function() {
+  console.log('Server started on port 7777');
 });
